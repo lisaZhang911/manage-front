825f3301860aea00e0c3856578091fb37e67af7f
myrcella <549521498@qq.com>
2020-04-13 17:27:40 +0800

    注册页面表单验证功能

--- src/service/loginService.js
+++ src/service/loginService.js
@@ -4,9 +4,11 @@ const getCode = (sid) => {
     return axios.get('/public/getCode',{ params:{sid: sid} })
 }
 const login =  (userInfo) =>  axios.post('/login',{...userInfo})
+const registry =  (userInfo) =>  axios.post('/reg',{...userInfo})
 
 
 export {
   getCode,
-  login
+  login,
+  registry
 }
--- src/views/Reg.vue
+++ src/views/Reg.vue
@@ -14,51 +14,72 @@
           <div class="layui-form layui-tab-content" id="LAY_ucm" style="padding: 20px 0;">
             <div class="layui-tab-item layui-show">
               <div class="layui-form layui-form-pane">
-                <form method="post">
+                <ValidationObserver ref="observer" v-slot="{validate}">
+                  <form method="post"  @submit.prevent="validate(submit)">
+                    <ValidationProvider name="email" rules="required|email" v-slot="{errors}">
+                      <div class="layui-form-item">
+                        <label for="L_email" class="layui-form-label">邮箱</label>
+                        <div class="layui-input-block">
+                          <input v-model="email" type="text" id="L_email" name="email" class="layui-input" placeholder="将会成为您唯一的登入名">
+                        </div>
+                        <div class="error layui-form-mid">{{errors[0]}}</div>
+                      </div>
+                    </ValidationProvider>
+                    <ValidationProvider name="name" rules="required" v-slot="{errors}">
+                      <div class="layui-form-item">
+                        <label for="L_username" class="layui-form-label">昵称</label>
+                        <div class="layui-input-block">
+                          <input v-model="nickName" type="text" id="L_username" name="name" class="layui-input">
+                        </div>
+                        <div class="error layui-form-mid">{{errors[0]}}</div>
+                      </div>
+                    </ValidationProvider>
+                    <ValidationProvider name="password" rules="required|min:6|max:16|confirmed:repass" v-slot="{errors}">
+                      <div class="layui-form-item">
+                        <label for="L_pass" class="layui-form-label">密码</label>
+                        <div class="layui-input-inline">
+                          <input v-model="password" type="password" id="L_pass" name="password" class="layui-input">
+                        </div>
+                        <div class="layui-form-mid layui-word-aux">6到16个字符</div>
+                      </div>
+                      <div class="error layui-form-mid">{{errors[0]}}</div>
+                    </ValidationProvider>
+                    <ValidationProvider name="repass" v-slot="{errors}"  vid="repass" rules="required|min:6|max:16" >
+                      <div class="layui-form-item">
+                        <label for="L_repass" class="layui-form-label">确认密码</label>
+                        <div class="layui-input-inline">
+                          <input v-model="repass" type="password" id="L_repass" name="repass" class="layui-input">
+                        </div>
+                      </div>
+                      <div class="error layui-form-mid">{{errors[0]}}</div>
+                    </ValidationProvider>
+                    <ValidationProvider name="code" rules="required|length:4" v-slot="{errors}">
+                      <div class="layui-form-item">
+                        <label for="L_vercode" class="layui-form-label">验证码</label>
+                        <div class="layui-input-inline">
+                          <input v-model="code" type="text" id="L_vercode" name="code" class="layui-input">
+                        </div>
+                        <div class="layui-form-mid">
+                          <span v-html="svg" @click="_getCaptcha">1</span>
+                        </div>
+                      </div>
+                      <div class="error layui-form-mid">{{errors[0]}}</div>
+                    </ValidationProvider>
                   <div class="layui-form-item">
-                    <label for="L_email" class="layui-form-label">邮箱</label>
-                    <div class="layui-input-inline">
-                      <input type="text" id="L_email" name="email" required lay-verify="email" autocomplete="off" class="layui-input">
-                    </div>
-                    <div class="layui-form-mid layui-word-aux">将会成为您唯一的登入名</div>
-                  </div>
-                  <div class="layui-form-item">
-                    <label for="L_username" class="layui-form-label">昵称</label>
-                    <div class="layui-input-inline">
-                      <input type="text" id="L_username" name="username" required lay-verify="required" autocomplete="off" class="layui-input">
-                    </div>
-                  </div>
-                  <div class="layui-form-item">
-                    <label for="L_pass" class="layui-form-label">密码</label>
-                    <div class="layui-input-inline">
-                      <input type="password" id="L_pass" name="pass" required lay-verify="required" autocomplete="off" class="layui-input">
-                    </div>
-                    <div class="layui-form-mid layui-word-aux">6到16个字符</div>
-                  </div>
-                  <div class="layui-form-item">
-                    <label for="L_repass" class="layui-form-label">确认密码</label>
-                    <div class="layui-input-inline">
-                      <input type="password" id="L_repass" name="repass" required lay-verify="required" autocomplete="off" class="layui-input">
-                    </div>
-                  </div>
-                  <div class="layui-form-item">
-                    <label for="L_vercode" class="layui-form-label">人类验证</label>
-                    <div class="layui-input-inline">
-                      <input type="text" id="L_vercode" name="vercode" required lay-verify="required" placeholder="请回答后面的问题" autocomplete="off" class="layui-input">
-                    </div>
-                    <div class="layui-form-mid">
-                      <span style="color: #c00;">1</span>
-                    </div>
-                  </div>
-                  <div class="layui-form-item">
-                    <button class="layui-btn" lay-filter="*" lay-submit>立即注册</button>
+                    <button class="layui-btn"  @click="submit">立即注册</button>
                   </div>
                   <div class="layui-form-item fly-form-app">
                     <span>或者直接使用社交账号快捷注册</span>
                     <a href="" onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-qq" title="QQ登入"></a>
                     <a href="" onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-weibo" title="微博登入"></a>
                   </div>
+                  <input v-validate="'required'" name="password" type="password" placeholder="Password" ref="password1">
+<span v-show="errors.has('password')" class="help is-danger">{{ errors.first('password') }}</span>
+
+<input v-validate="'required|confirmed:password'" name="password_confirmation" type="password" placeholder="Password, Again">
+<span v-show="errors.has('password_confirmation')" class="help is-danger">{{ errors.first('password_confirmation') }}</span>
                 </form>
+              </ValidationObserver>
               </div>
             </div>
           </div>
@@ -69,7 +90,75 @@
 </template>
 
 <script>
+import { getCode, registry } from '../service/loginService.js'
+import { v4 as uuidv4 } from 'uuid'
+import { ValidationObserver,ValidationProvider } from 'vee-validate'
+
 export default {
+  components:{
+    ValidationObserver,
+    ValidationProvider
+  },
+  data(){
+    return {
+      svg:'',
+      email:'',
+      nickName:'',
+      password:'',
+      repass:'',
+      code:''
+    }
+  },
+  methods:{
+    _getCaptcha(){
+      let sid = this.$store.state.sid
+      getCode(sid).then(r => {this.svg = r.data})
+    },
+    async submit(){
+      console.log(isValid);
+      const isValid = await this.$refs.observer.validate()
+      console.log(isValid);
+      if(!isValid){
+        return
+      }
+      registry({
+        email: this.email,
+        name: this.nickName,
+        password: this.password,
+        code: this.code,
+        sid: this.$store.state.sid
+      })
+    }
+  },
+  mounted(){
+    let sid = ''
+    if(localStorage.getItem('sid')){
+      sid = localStorage.getItem('sid')
+    } else {
+      sid = uuidv4()
+      localStorage.setItem('sid',sid)
+    }
+    this.$store.commit('setSid',sid)
+    this._getCaptcha()
+
+    const dict = {
+      custom: {
+        email: {
+          required: '邮箱不能为空'
+        },
+        name: {
+          required: '昵称不能为空'
+        },
+        password: {
+          required: '密码必填'
+        },
+        repass:{
+          required: '密码必填'
+        }
+      }
+    };
+    this.$validator.localize('zh-CN', dict);
+  }
 }
 </script>
 
